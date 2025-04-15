16366058390f22ba0cf914f43eea913a6db0a926
Joao Pedro Afonso Cerqueira <114918447+jpacerqueira@users.noreply.github.com>
2025-04-15 15:50:00 +0100

    Update run_llm-dev-container_docker.sh

--- llm-dev-container/run_llm-dev-container_docker.sh
+++ llm-dev-container/run_llm-dev-container_docker.sh
@@ -5,16 +5,21 @@ CURRENT_DIR=$(pwd)
 
 # Build the Docker image
 echo "Building Docker image..."
-docker build -t llm-dev-env -f python311.Dockerfile .
+docker build -t llm-dev-env -f python312.Dockerfile .
 
-# Run the Docker container with volume mounting
+# Run the Docker container with volume mounting and port mapping
 echo "Starting Docker container..."
 docker run -it \
     -p 8888:8888 \
+    -p 5000-5009:5000-5009 \
+    -p 8000-8009:8000-8009 \
+    -p 8501-8509:8501-8509 \
+    -p 9999:9999 \
     -v "${CURRENT_DIR}:/app/my_apps" \
     --name llm-dev-container \
     llm-dev-env
 
 # If the container was stopped, remove it
 echo "Cleaning up..."
-docker rm llm-dev-container 
+docker stop llm-dev-container
+#docker rm llm-dev-container
